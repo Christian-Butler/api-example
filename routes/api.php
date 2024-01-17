626f5322af8faad63eea9e440fbea4430692f701
Christian Butler <N00211989@iadt.ie>
2024-01-17 16:43:14 +0000

    fda

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth/user', [AuthController::class, 'user']);
+});
